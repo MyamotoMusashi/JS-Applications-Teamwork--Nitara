f2838e9c97befe4af95954f13b3986c4627dd9f7
emilrr <emil.ruzhenov@gmail.com>
2016-10-01 23:48:29 +0300

    Added sort of cars

--- scripts/grid-plugin.js
+++ scripts/grid-plugin.js
@@ -1,21 +1,36 @@
-import { cars } from '../db/db.js';
+import {cars} from '../db/db.js';
 
-var grid = (function() {
-    function showGrid(pageNumber) {
+var grid = (function () {
+
+    let $containerFluid = $('.container-fluid');
+
+    function showGrid(pageNumber, keySorted) {
         let $gridContainer = $('<div id="grid-container"></div>');
+
         let showedCarsInGrid = 12;
         pageNumber = pageNumber || 0;
+        let sortedKey = keySorted || 'Brand';
 
         Promise.all([cars.getAllFreeCars(), $.get('../templates/grid-gallery.handlebars')])
             .then(([cars, data]) => {
                 let carsLen = cars.length;
 
+                function sortByKey(array, key) {
+                    return array.sort(function (a, b) {
+                        var x = a[key];
+                        var y = b[key];
+                        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+                    });
+                }
+
+                cars = sortByKey(cars, sortedKey);
+
                 cars = cars.splice(showedCarsInGrid * pageNumber, showedCarsInGrid);
                 let template = Handlebars.compile(data);
                 let html = template(cars);
                 $gridContainer.html(html);
-                $('#content').html('');
-                $('#content').append($gridContainer);
+                $('#grid-container').html('');
+                $('#grid-container').append($gridContainer);
                 return carsLen / showedCarsInGrid;
             })
             .then((length) => {
@@ -34,15 +49,41 @@ var grid = (function() {
             .then(() => {
                 $.get('../templates/grid-sorting.handlebars', (data) => {
                     let template = Handlebars.compile(data);
+                    $("#pricePerDay").on('click', function () {
+                        alert("success");
+                    });
 
                     $('#grid-pagination').html(template);
                 });
             });
-
-
     }
 
-    $('#grid-container').on('click', '#btn-page', function() {
+    $containerFluid.on('click', '#sortByPricePerDay', function () {
+        let pageIndex = $('#grid-container').attr('data-index') - 1;
+        showGrid(pageIndex, 'PricePerDay');
+    });
+
+    $containerFluid.on('click', '#sortBySeats', function () {
+        let pageIndex = $('#grid-container').attr('data-index') - 1;
+        showGrid(pageIndex, 'Seats');
+    });
+
+    $containerFluid.on('click', '#sortByFuel', function () {
+        let pageIndex = $('#grid-container').attr('data-index') - 1;
+        showGrid(pageIndex, 'Fuel');
+    });
+
+    $containerFluid.on('click', '#sortByGears', function () {
+        let pageIndex = $('#grid-container').attr('data-index') - 1;
+        showGrid(pageIndex, 'Gears');
+    });
+
+    $containerFluid.on('click', '#sortByYear', function () {
+        let pageIndex = $('#grid-container').attr('data-index') - 1;
+        showGrid(pageIndex, 'Year');
+    });
+
+    $('#grid-container').on('click', '#btn-page', function () {
         let pageIndex = $(this).attr('data-index') - 1;
         showGrid(pageIndex);
     });
@@ -57,4 +98,4 @@ var grid = (function() {
     };
 }());
 
-export { grid }
+export {grid}
